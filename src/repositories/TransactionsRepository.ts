a8085a96bc5cdfe278c7869821df2a39d9e3407d
GUILHERME VICENTE DE SOUZA <U04329@pioxii.com.br>
2020-04-24 16:50:01 -0300

    Repository/routes/Services changes

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,29 +1,41 @@
-import Transaction from '../models/Transaction';
+import Transaction from '../models/Transaction'
 
 interface Balance {
-  income: number;
-  outcome: number;
-  total: number;
+  income: number
+  outcome: number
+  total: number
+}
+
+interface CreateTransactionDTO {
+  title: string
+  value: number
+  type: 'income' | 'outcome'
 }
 
 class TransactionsRepository {
-  private transactions: Transaction[];
+  private transactions: Transaction[]
 
   constructor() {
-    this.transactions = [];
+    this.transactions = []
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions
   }
 
+  /*
   public getBalance(): Balance {
-    // TODO
+    return
   }
+  */
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type })
+
+    this.transactions.push(transaction)
+
+    return transaction
   }
 }
 
-export default TransactionsRepository;
+export default TransactionsRepository
